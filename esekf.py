e656352b32d9194aebe90d3822384791f7873e2e
Mingfeng Zhang <mingfeng.zhang@outlook.com>
2022-02-03 23:40:20 -0500

    updated comments

--- esekf.py
+++ esekf.py
@@ -4,32 +4,49 @@ from rotations import angle_normalize, rpy_jacobian_axis_angle, skew_symmetric, 
 
 class ESEKF:
     def __init__(self, p_0=None, v_0=None, q_0=None):
-        self.p = np.zeros(shape=(1, 3)) if p_0 is None else p_0
-        self.v = np.zeros(shape=(1, 3)) if v_0 is None else v_0
-        self.q = Quaternion(1.0, 0.0, 0.0, 0.0) if q_0 is None else q_0
-        self.g = np.array([0, 0, -9.81])
-        self.a_b = 0
-        self.w_b = 0
-        self.F = np.eye(9)
-        self.Q = np.zeros((6, 6))
-        self.p_cov = np.zeros(9)
-        self.l_jac = np.zeros([9, 6])
+        """
+        Initialization method to set initial states and other variables
+        :param p_0: initial position
+        :param v_0: initial velocity
+        :param q_0: initial orientation in the format of quaternion
+        """
+        self.p = np.zeros(shape=(1, 3)) if p_0 is None else p_0  # estimated position
+        self.v = np.zeros(shape=(1, 3)) if v_0 is None else v_0  # estimated velocity
+        self.q = Quaternion(1.0, 0.0, 0.0, 0.0) if q_0 is None else q_0  # estimated orientation in quaternion
+        self.g = np.array([0, 0, -9.81])  # vectorized acceleration of gravity
+        self.a_b = 0  # accelerometer bias
+        self.w_b = 0  # gyrometer bias
+        self.F = np.eye(9)  # jacobian matrix of error-state kinematics w.r.t. the error state
+        self.Q = np.zeros((6, 6))  # Covariance matrix of the perturbation impulses
+        self.p_cov = np.zeros((9, 9))  # Covariance matrix of the error-state
+
+        self.l_jac = np.zeros((9, 6)) # jacobian matrix of error-state kinematics w.r.t. the perturbation impulses vector
         self.l_jac[3:, :] = np.eye(6)  # motion model noise jacobian
-        self.h_jac = np.zeros([3, 9])
+
+        self.h_jac = np.zeros((3, 9)) # jacobian matrix of position observation w.r.t to nominal state
         self.h_jac[:, :3] = np.eye(3)  # measurement model jacobian
-        self.var_imu_f = 0.10
-        self.var_imu_w = 0.25
+        self.var_imu_f = 0.10  # variance of force/acceleration measurements (i.e., accelerometer)
+        self.var_imu_w = 0.25  # variance of rotational velocity measurement (i.e., gyrometer)
 
     def kinematics(self, a_m, w_m, dt):
+        """
+        Prediction step for nominal states and error states
+        :param a_m: acceleration measurement
+        :param w_m: rotational velocity measurement
+        :param dt: time interval to the next time step
+        :return:
+        """
         assert (dt > 0)
-        R = self.q.to_mat()
-        acce = R.dot(a_m - self.a_b)
-        a = acce + self.g
+        R = self.q.to_mat()  # obtain current orientation in the format of a rotational matrix
+        acce = R.dot(a_m - self.a_b)  # transform acceleration measurement from vehicle frame to world frame
+        a = acce + self.g  # add gravity vector
 
+        # Predict the next position, velocity & orientation by integrating the high-rate IMU measurements
         self.p += self.v*dt + 0.5*a*dt*dt
         self.v += a*dt
         self.q = self.q.quat_mult_left(Quaternion(axis_angle=dt * (w_m-self.w_b)), 'Quaternion')
 
+        # Predict the covariance matrix of the next error-state
         self.F[0:3, 3:6] = np.eye(3) * dt
         self.F[3:6, 6:9] = -skew_symmetric(acce) * dt
         self.Q[0:3, 0:3] = np.eye(3) * self.var_imu_f * (dt ** 2)
@@ -38,26 +55,39 @@ class ESEKF:
         self.p_cov = self.F @ self.p_cov @ self.F.T + self.l_jac @ self.Q @ self.l_jac.T
 
     def measurement_update(self, sensor_var, y_k):
-        # 3.1 Compute Kalman Gain
+        """
+        Correct the predicted states & covariance by incorporating external measurments
+        :param sensor_var: variance of the external sensor (Lidar or GNSS) measurements
+        :param y_k: position measurement by the external sensor (Lidar or GNSS)
+        :return:
+        """
+        # Compute Kalman Gain
         K = self.p_cov @ self.h_jac.T @ np.linalg.inv(self.h_jac @ self.p_cov @ self.h_jac.T + sensor_var)
-        # 3.2 Compute error state
+
+        # Compute error state
         delta_x = K.dot(y_k - self.p)
-        # 3.3 Correct predicted state
+
+        # Correct predicted state
         self.p += delta_x[0:3]
         self.v += delta_x[3:6]
         delta_quat = Quaternion(axis_angle=delta_x[6:9])
-        # print("q_axis = {}".format( Quaternion(axis_angle = delta_x[6:9]) ))
-        # print("q_euler = {}".format( Quaternion(euler = delta_x[6:9]) ))
         self.q = delta_quat.quat_mult_left(self.q, 'Quaternion')
-        # 3.4 Compute corrected covariance
+
+        # Compute corrected covariance
         self.p_cov = (np.eye(9) - (K @ self.h_jac)) @ self.p_cov
 
     def get_states(self):
+        """
+        Return the estimated states
+        """
         return self.p, self.v, self.q, self.p_cov
 
     def set_imu_var(self, var_f, var_m):
+        """
+        Set the variance of accelerometer and gyrometer
+        :param var_f:
+        :param var_m:
+        :return:
+        """
         self.var_imu_f = var_f  # 0.10
         self.var_imu_w = var_m  # 0.25
-
-
-
--- test_esekf.py
+++ test_esekf.py
@@ -1,22 +1,32 @@
 import pickle
+import sys
+
 import numpy as np
 from esekf import ESEKF
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from rotations import angle_normalize, rpy_jacobian_axis_angle, skew_symmetric, Quaternion
 
+from rotations import angle_normalize, rpy_jacobian_axis_angle, Quaternion
+
+
+################################################################################################
+# Transform the LIDAR data to the IMU frame using a known extrinsic calibration
+# rotation matrix C_li and translation vector t_i_li.
 # Correct calibration rotation matrix, corresponding to Euler RPY angles (0.05, 0.05, 0.1).
+################################################################################################
+
 C_li = np.array([
    [0.99376, -0.09722,  0.05466],
    [0.09971,  0.99401, -0.04475],
    [-0.04998,  0.04992,  0.9975]
 ])
+
 t_i_li = np.array([0.5, 0.1, 0.5])
 
+
 def main():
-    # load data from file
+    # load data from file into a dictionary
     with open('data/pt3_data.pkl', 'rb') as file:
-        data = pickle.load(file)
+        data = pickle.load(file)  # data's type is dictionary
 
     ################################################################################################
     # Each element of the data dictionary is stored as an item from the data dictionary, which we
@@ -50,33 +60,31 @@ def main():
     lidar.data = (C_li @ lidar.data.T).T + t_i_li
 
     ################################################################################################
-    # Now that our data is set up, we can start getting things ready for our solver. One of the
-    # most important aspects of a filter is setting the estimated sensor variances correctly.
-    # We set the values here.
+    # Setting the estimated sensor variances
     ################################################################################################
-    var_imu_f = 0.10
-    var_imu_w = 0.25
-    var_gnss = 0.01
-    var_lidar = 1.00
+    var_imu_f = 0.01  # 0.10
+    var_imu_w = 0.01  # 0.25
+    var_gnss = 0.1  # 0.01
+    var_lidar = 35   # 1.00
 
     ################################################################################################
-    # Let's set up some initial values for our ES-EKF solver.
+    # Create arrays to store estimated states and covariance.
     ################################################################################################
     p_est = np.zeros([imu_f.data.shape[0], 3])  # position estimates
     v_est = np.zeros([imu_f.data.shape[0], 3])  # velocity estimates
     q_est = np.zeros([imu_f.data.shape[0], 4])  # orientation estimates as quaternions
-    p_cov = np.zeros([imu_f.data.shape[0], 9, 9])  # covariance matrices at each timestep
+    p_cov = np.zeros([imu_f.data.shape[0], 9, 9])  # covariance matrices at each time step
 
-    # Set initial values.
+    ################################################################################################
+    # Set initial values
+    ################################################################################################
     p_est[0] = gt.p[0]
     v_est[0] = gt.v[0]
     q0 = Quaternion(euler=gt.r[0])
     q_est[0] = q0.to_numpy()
     p_cov[0] = np.zeros(9)  # covariance of estimate
-    gnss_i = 0
-    lidar_i = 0
-
-    #### 5. Main Filter Loop #######################################################################
+    # gnss_i = 0
+    # lidar_i = 0
 
     ################################################################################################
     # Now that everything is set up, we can start taking in the sensor data and creating estimates
@@ -89,23 +97,36 @@ def main():
     while gnss.t[k_g] < imu_f.t[1]:
         k_g += 1
 
-    es_efk_solver = ESEKF(p_0=p_est[0], v_0=v_est[0], q_0=q0)
+    ################################################################################################
+    # Create an ESEKF object with initial states
+    # Set the sensor variances
+    ################################################################################################
+    es_efk_solver = ESEKF(np.copy(gt.p[0]), np.copy(gt.v[0]), q0)
     es_efk_solver.set_imu_var(var_f=var_imu_f, var_m=var_imu_w)
 
-    for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial prediction from gt
+    ################################################################################################
+    # Start taking in the sensor data and creating estimates for the states in a loop
+    ################################################################################################
+    for k in range(1, imu_f.data.shape[0]):  # start at 1 because we have initial prediction from gt
         delta_t = imu_f.t[k] - imu_f.t[k - 1]
-        es_efk_solver.kinematics(a_m=imu_f.data[k], w_m=imu_w.data[k], dt=delta_t)
 
+        # Prediction step of the nominal & error states. This happens at a high-rate
+        es_efk_solver.kinematics(a_m=imu_f.data[k-1], w_m=imu_w.data[k-1], dt=delta_t)
+
+        # If there is no external measurement to correct the predicted state, the predicted one will be considered
+        # as estimated state
         p_hat, v_hat, q_hat, p_cov_hat = es_efk_solver.get_states()
         p_est[k] = p_hat
         v_est[k] = v_hat
         q_est[k] = q_hat.to_numpy()
         p_cov[k] = p_cov_hat
 
+        # Correction by Lidar measurements. This happens at a lower rate
         if k_l < lidar.data.shape[0] and k < imu_f.data.shape[0] - 1 and lidar.t[k_l] >= imu_f.t[k] and lidar.t[k_l] < \
                 imu_f.t[k + 1]:
             delta_tl = 1.0  # lidar.t[k_l] - lidar.t[k_l -1]
             es_efk_solver.measurement_update(np.eye(3) * var_lidar * (delta_tl ** 2), lidar.data[k_l])
+
             p_hat, v_hat, q_hat, p_cov_hat = es_efk_solver.get_states()
             p_est[k] = p_hat
             v_est[k] = v_hat
@@ -113,10 +134,12 @@ def main():
             p_cov[k] = p_cov_hat
             k_l += 1
 
+        # Correction by GNSS measurements. This happens at a lower rate
         if k_g < gnss.data.shape[0] and k < imu_f.data.shape[0] - 1 and gnss.t[k_g] >= imu_f.t[k] and gnss.t[k_g] < \
                 imu_f.t[k + 1]:
             delta_tg = 1.0  # gnss.t[k_g] - gnss.t[k_g -1]
             es_efk_solver.measurement_update(np.eye(3) * var_gnss * (delta_tg ** 2), gnss.data[k_g])
+
             p_hat, v_hat, q_hat, p_cov_hat = es_efk_solver.get_states()
             p_est[k] = p_hat
             v_est[k] = v_hat
@@ -125,14 +148,11 @@ def main():
             k_g += 1
 
     ################################################################################################
-    # Now that we have state estimates for all of our sensor data, let's plot the results. This plot
-    # will show the ground truth and the estimated trajectories on the same plot. Notice that the
-    # estimated trajectory continues past the ground truth. This is because we will be evaluating
+    # Plot the results to show the ground truth and the estimated trajectories on the same plot.
+    # Notice that the estimated trajectory continues past the ground truth. This is because we will be evaluating
     # your estimated poses from the part of the trajectory where you don't have ground truth!
     ################################################################################################
-    #print(p_est)
-    # print(p_est[-2, :])
-    print(p_est[-160:-140,:])
+
     est_traj_fig = plt.figure()
     ax = est_traj_fig.add_subplot(111, projection='3d')
     ax.plot(p_est[:, 0], p_est[:, 1], p_est[:, 2], label='Estimated')
@@ -151,48 +171,51 @@ def main():
     ax.view_init(elev=45, azim=-50)
 
     ################################################################################################
-    # We can also plot the error for each of the 6 DOF, with estimates for our uncertainty
-    # included. The error estimates are in blue, and the uncertainty bounds are red and dashed.
+    # Plot the error for each of the 6 DOF, with estimates for the uncertainty included.
+    # The error estimates are in blue, and the uncertainty bounds are red and dashed.
     # The uncertainty bounds are +/- 3 standard deviations based on our uncertainty (covariance).
     ################################################################################################
-    # error_fig, ax = plt.subplots(2, 3)
-    # error_fig.suptitle('Error Plots')
-    # num_gt = gt.p.shape[0]
-    # p_est_euler = []
-    # p_cov_euler_std = []
-    #
-    # # Convert estimated quaternions to euler angles
-    # for i in range(len(q_est)):
-    #     qc = Quaternion(*q_est[i, :])
-    #     p_est_euler.append(qc.to_euler())
-    #
-    #     # First-order approximation of RPY covariance
-    #     J = rpy_jacobian_axis_angle(qc.to_axis_angle())
-    #     p_cov_euler_std.append(np.sqrt(np.diagonal(J @ p_cov[i, 6:, 6:] @ J.T)))
-    #
-    # p_est_euler = np.array(p_est_euler)
-    # p_cov_euler_std = np.array(p_cov_euler_std)
-    #
-    # # Get uncertainty estimates from P matrix
-    # p_cov_std = np.sqrt(np.diagonal(p_cov[:, :6, :6], axis1=1, axis2=2))
-    #
-    # titles = ['Easting', 'Northing', 'Up', 'Roll', 'Pitch', 'Yaw']
-    # for i in range(3):
-    #     ax[0, i].plot(range(num_gt), gt.p[:, i] - p_est[:num_gt, i])
-    #     ax[0, i].plot(range(num_gt), 3 * p_cov_std[:num_gt, i], 'r--')
-    #     ax[0, i].plot(range(num_gt), -3 * p_cov_std[:num_gt, i], 'r--')
-    #     ax[0, i].set_title(titles[i])
-    # ax[0, 0].set_ylabel('Meters')
-    #
-    # for i in range(3):
-    #     ax[1, i].plot(range(num_gt), \
-    #                   angle_normalize(gt.r[:, i] - p_est_euler[:num_gt, i]))
-    #     ax[1, i].plot(range(num_gt), 3 * p_cov_euler_std[:num_gt, i], 'r--')
-    #     ax[1, i].plot(range(num_gt), -3 * p_cov_euler_std[:num_gt, i], 'r--')
-    #     ax[1, i].set_title(titles[i + 3])
-    # ax[1, 0].set_ylabel('Radians')
+    error_fig, ax = plt.subplots(2, 3)
+    error_fig.suptitle('Error Plots')
+    num_gt = gt.p.shape[0]
+    p_est_euler = []
+    p_cov_euler_std = []
+
+    # Convert estimated quaternions to euler angles
+    for i in range(len(q_est)):
+        qc = Quaternion(*q_est[i, :])
+        p_est_euler.append(qc.to_euler())
+
+        # First-order approximation of RPY covariance
+        J = rpy_jacobian_axis_angle(qc.to_axis_angle())
+        p_cov_euler_std.append(np.sqrt(np.diagonal(J @ p_cov[i, 6:, 6:] @ J.T)))
+
+    p_est_euler = np.array(p_est_euler)
+    p_cov_euler_std = np.array(p_cov_euler_std)
+
+    # Get uncertainty estimates from P matrix
+    p_cov_std = np.sqrt(np.diagonal(p_cov[:, :6, :6], axis1=1, axis2=2))
+
+    titles = ['Easting', 'Northing', 'Up', 'Roll', 'Pitch', 'Yaw']
+    for i in range(3):
+        ax[0, i].plot(range(num_gt), gt.p[:, i] - p_est[:num_gt, i])
+        ax[0, i].plot(range(num_gt), 3 * p_cov_std[:num_gt, i], 'r--')
+        ax[0, i].plot(range(num_gt), -3 * p_cov_std[:num_gt, i], 'r--')
+        ax[0, i].set_title(titles[i])
+    ax[0, 0].set_ylabel('Meters')
+
+    for i in range(3):
+        ax[1, i].plot(range(num_gt),
+                      angle_normalize(gt.r[:, i] - p_est_euler[:num_gt, i]))
+        ax[1, i].plot(range(num_gt), 3 * p_cov_euler_std[:num_gt, i], 'r--')
+        ax[1, i].plot(range(num_gt), -3 * p_cov_euler_std[:num_gt, i], 'r--')
+        ax[1, i].set_title(titles[i + 3])
+    ax[1, 0].set_ylabel('Radians')
     plt.show()
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("Whew!", sys.exc_info()[0], "occurred")
